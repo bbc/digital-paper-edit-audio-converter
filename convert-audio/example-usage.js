9c9f63366d60613c456e265df4739484ece9f690
Pietro Passarelli - News Labs <pietro.passarelli@bbc.co.uk>
2019-05-23 16:00:54 +0100

    first commit

---
+++ convert-audio/example-usage.js
@@ -0,0 +1,7 @@
+const convertToWav = require('./index.js');
+const url = 'https://download.ted.com/talks/KateDarling_2018S-950k.mp4';
+const audioFileOutput = './test2.ogg';
+
+convertToWav(url, audioFileOutput, function(newFile) {
+    console.log(newFile)
+})
---
+++ convert-audio/index.js
@@ -0,0 +1,50 @@
+// originally from https://github.com/OpenNewsLabs/autoEdit_2/blob/master/lib/interactive_transcription_generator/transcriber/convert_to_audio.js
+
+/**
+* @module convertToWav
+* @description Convers video file into wav (or any ffmpeg supported input)
+* takes in input file, output destination file, and callback,
+* IBM sudgested audio compression {@link http://www.ibm.com/watson/developercloud/doc/speech-to-text/input.shtml#limits}
+* callback returns audio file name/path
+* @example <caption>usage</caption>
+var convertToWav = require('./index.js');
+  
+convertToWav(file, audioFile, function(newFile) {
+  //do something with the converted file, returned as a string file path 
+}
+* @requires fluent-ffmpeg
+*/
+// "use strict";
+
+const ffmpegBin = require('ffmpeg-static-electron');
+const ffmpegBinPath = ffmpegBin.path;
+const ffmpeg = require('fluent-ffmpeg');
+    ffmpeg.setFfmpegPath(ffmpegBinPath)
+
+/**
+ * @function convertToWav
+ * @param {string} file -  path to audio or viceo file to convert to wav
+ * @param {string} audioFileOutput - path to output ogg audio file
+ * @returns {callback} callback - callback to return audio file path as string. 
+*/
+function convertToWav(file, audioFileOutput, callback) {
+console.log(file)
+  //running ffmpeg comand 
+  var comand = ffmpeg(file)
+                .noVideo()
+                .audioCodec('libopus')
+                .audioChannels(1)
+                .audioFrequency(16000)
+                .output(audioFileOutput)
+                .on('end',
+                //listener must be a function, so to return the callback wrapping it inside a function
+                  function(){
+                    callback(audioFile)
+                  }
+                  || function() {
+                     console.log('Finished processing');
+                  }
+                ).run();
+}
+
+module.exports = convertToWav;

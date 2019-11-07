b6df9e8f43f02d56eb8998662e464f4a79f3fcb0
Eimi Okuno <eimi.okuno@bbc.co.uk>
2019-11-07 19:17:07 +0000

    Downloadable endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,21 @@
 const functions = require("firebase-functions");
 const { convertToAudio } = require("@bbc/convert-to-audio");
 
-// const url = "https://download.ted.com/talks/KateDarling_2018S-950k.mp4";
-// const audioFileOutput = "./ted-talk.wav";
+// const input = "https://download.ted.com/talks/KateDarling_2018S-950k.mp4";
+// const output = "./ted-talk.wav";
 
 const convert = async (request, response) => {
   const payload = request.body;
   if (payload) {
     try {
-      const audioFile = await convertToAudio(payload.url, payload.output);
-      response.send(`Converting audio ${payload.url} to ${audioFile}`);
+      const audioFile = await convertToAudio(payload.input, payload.output);
+      response.download(audioFile);
     } catch (err) {
       console.error(err);
-      response.send(`There was a problem processing your audio file: ${err}`);
+      response.send(
+        `There was a problem processing your audio file: 
+        ${err}. Passed payload:${JSON.stringify(payload)}`
+      );
     }
   } else {
     response.send("No URL given");
